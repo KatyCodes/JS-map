1de3ceb8f74fe17a852694295ee9f37771179756
Katy Henning and Ryan Loos <katy+ryan@>
2016-10-12 13:49:34 -0700

    made it wurk

---
+++ js/Maps.js
@@ -0,0 +1,16 @@
+var apiKey = require('./../.env').apiKey;
+// 
+// Maps = function() {
+//   function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: -34.397, lng: 150.644},
+//           zoom: 8
+//         });
+//       }
+//     </script>
+//     <script src="https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap"
+//     async defer></script>
+// };
+//
+//
+// exports.mapsModule = Maps;
