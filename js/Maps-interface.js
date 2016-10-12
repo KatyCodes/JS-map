a429545605144dab7414965ce08aa169202a718c
Katy Henning and Ryan Loos <katy@>
2016-10-12 15:55:58 -0700

    added ability to go to new locations

--- js/Maps-interface.js
+++ js/Maps-interface.js
@@ -1,16 +1,29 @@
-var Maps = require('./../js/maps.js').MapsModule;
+// var Maps = require('./../js/maps.js').MapsModule;
 var apiKey = require('./../.env').apiKey;
 
-function makeURL() {
-  var url = 'https://maps.googleapis.com/maps/api/js?key=' + apiKey;
-  return url;
-}
-
 $( document ).ready(function() {
-  $('#locateUser').click(locateUser);
   $('head').append('<script src=\"https://maps.googleapis.com/maps/api/js?key=' + apiKey + '\" async defer></script>');
+  $('#locate_User').click(locateUser);
+  $('#user_input').submit(function(event) {
+    event.preventDefault();
+    var userlatitude = $('#latitude').val();
+    var userlongitude = $('#longitude').val();
+    // var Newpoint = new NewMap();
+    // console.log(Newpoint);
+    var userLatLng = new google.maps.LatLng(userlatitude, userlongitude);
+    var myOptions = {
+      zoom : 5,
+      center : userLatLng,
+      mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+    var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+    // Place the marker
+    new google.maps.Marker({
+      map: mapObject,
+      position: userLatLng
+    });
+  });
 });
-
 
 //google maps functions
 function locateUser() {
@@ -27,36 +40,44 @@ function locateUser() {
   }
 }
 
-
+function newLocation(lat, long) {
+  var newLocation = new google.maps.LatLng(lat, long);
+  return newLocation;
+}
 
 // this is the success callback from telling the navigator (your browser) to get the current user's position
 // we do this on line 13 above. We pass in a function to call on success, a function to call on error, and some options to tell the geolocation api how we want it to run.
 // on successfully locating the user, geolocationSuccess() gets called automatically, and it is passed the user's position as an argument.
 // on error, geolocationError is called.
 
-
 function geolocationSuccess(position) {
-  // here we take the `position` object returned by the geolocation api
-  // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
-  // it 2 arguments: one for latitude, one for longitude.
-  // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
-  // But you must use coordinates to use this method.
-  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // here we take the `position` object returned by the geolocation api
+    // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
+    // it 2 arguments: one for latitude, one for longitude.
+    // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
+    // But you must use coordinates to use this method.
+    var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //  var newLocation = Newpoint.newLocation(userlatitude, userlongitude);
 
-  var myOptions = {
-    zoom : 16,
-    center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  };
-  // Draw the map - you have to use 'getElementById' here.
-  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-  // Place the marker
-  new google.maps.Marker({
-    map: mapObject,
-    position: userLatLng
-  });
+
+    var myOptions = {
+      zoom : 5,
+      center : userLatLng,
+      mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+    // Draw the map - you have to use 'getElementById' here.
+    var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+    // Place the marker
+    new google.maps.Marker({
+      map: mapObject,
+      position: userLatLng
+    });
+    // new google.maps.Marker({
+    //   map: mapObject,
+    //   position: newLocation
+    // });
 }
 
 function geolocationError(positionError) {
-  alert(positionError);
+    alert(positionError);
 }
